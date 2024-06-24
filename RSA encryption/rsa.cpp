038b0db0d80a249df84472151fff53ab7a86638b
AlfaSnitch <alfawarrior447@gmail.com>
2024-06-24 20:55:37 +0600

    new updates

--- RSA encryption/rsa.cpp
+++ RSA encryption/rsa.cpp
@@ -11,73 +11,86 @@ cpp_int getRandomR(cpp_int &n){
     return dist(gen);
 }
 
-cpp_int modInv(cpp_int a,cpp_int m){
-    cpp_int m0 = m;
+cpp_int modInv(cpp_int A, cpp_int M){
+    cpp_int m0 = M;
     cpp_int y=0,x=1;
 
-    if(m==1) return 0;
+    if(M==0) return 0;
 
-    while(a>1){
-        cpp_int q = a/m;
-        cpp_int t = m;
+    while(A>1){
+        cpp_int t=M;
+        cpp_int q = A/M;
 
-        m = a%m;
-        a = t;
+        M = A%M;
+        A = t;
         t = y;
 
         y = x - q*y;
         x = t;
     }
-    if(x<0){
-        x +=m0;
-    }
+    if(x<0) x+=m0;
     return x;
 }
 
-// b^p % mod = ??
-cpp_int power(cpp_int b,cpp_int p,cpp_int mod){
+cpp_int power(cpp_int b, cpp_int p,cpp_int mod){
     b = b%mod;
-    if(p == 0) return 1;
+    if(p==0) return 1;
+
     cpp_int j = power(b,p/2,mod);
 
-    j = (j*j)%mod;
+    j = (j*j) % mod;
     if(p%2==1){
-        j = (j*b)%mod;
+        j = (j*b) % mod;
     }
     return j;
 }
 
-cpp_int Egcd(cpp_int a,cpp_int b){
-    if(a == 0)
-        return b;
-    return gcd(b%a,a);
+cpp_int egcd(cpp_int a, cpp_int b){
+    if(a==0) return b;
+    return egcd(b%a,a);
 }
 
 int main(){
-    cpp_int e,d,phi,n;
+    cpp_int e,d,phi,m,n;
+
     cpp_int p("656692050181897513638241554199181923922955921760928836766304161790553989228223793461834703506872747071705167995972707253940099469869516422893633357693");
     cpp_int q("204616454475328391399619135615615385636808455963116802820729927402260635621645177248364272093977747839601125961863785073671961509749189348777945177811");
-
 
     n = p*q;
     phi = (p-1)*(q-1);
 
     e = getRandomR(phi);
-    while(Egcd(phi,e)!=1){
+    while(egcd(e,phi)!=1){
         e = getRandomR(phi);
     }
 
     d = modInv(e,phi);
 
-    cpp_int m = 10;
+    cpp_int enc,dec;
 
-    cpp_int sign,verify;
+    //message
+    m = 12;
 
-    sign = power(m,d,n);
+    //encryption
+    enc = power(m,e,n);
 
-    verify = power(sign,e,n);
+    //decryption
+    dec = power(enc,d,n);
 
-    cout<<sign<<endl<<verify<<endl;
+    cout<<enc<<endl<<dec<<endl;
 
-
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- RSA product cipher/rsa_product_cipher.cpp
+++ RSA product cipher/rsa_product_cipher.cpp
@@ -5,17 +5,22 @@
 using namespace std;
 using namespace boost::multiprecision;
 
-cpp_int getRandomR(cpp_int &n){
+cpp_int getRandomR(cpp_int n){
     boost::random::mt19937 gen(static_cast<unsigned int>(time(nullptr)));
     boost::random::uniform_int_distribution<cpp_int> dist(3,n-1);
     return dist(gen);
 }
 
-cpp_int modInv(cpp_int a,cpp_int m){
-    cpp_int m0 = m;
-    cpp_int y=0,x=1;
+cpp_int egcd(cpp_int a,cpp_int b){
+    if(a == 0) return b;
 
-    if(m==1) return 0;
+    return egcd(b%a,a);
+}
+
+cpp_int modInv(cpp_int a,cpp_int m){
+    cpp_int m0 = m,y=0,x=1;
+
+    if(m==0) return 0;
 
     while(a>1){
         cpp_int q = a/m;
@@ -28,56 +33,60 @@ cpp_int modInv(cpp_int a,cpp_int m){
         y = x - q*y;
         x = t;
     }
-    if(x<0){
-        x +=m0;
-    }
+    if(x<0) x+=m0;
+
     return x;
 }
 
-// b^p % mod = ??
-cpp_int power(cpp_int b,cpp_int p,cpp_int mod){
-    b = b%mod;
+cpp_int power(cpp_int b,cpp_int p, cpp_int mod){
+    b = b% mod;
     if(p == 0) return 1;
-    cpp_int j = power(b,p/2,mod);
 
-    j = (j*j)%mod;
+    cpp_int j = power(b,p/2,mod);
+    j = (j*j)% mod;
     if(p%2==1){
         j = (j*b)%mod;
     }
     return j;
 }
 
-cpp_int Egcd(cpp_int a,cpp_int b){
-    if(a == 0)
-        return b;
-    return gcd(b%a,a);
-}
-
-int main(){
-    cpp_int e,d,phi,n;
+int main()
+{
     cpp_int p("656692050181897513638241554199181923922955921760928836766304161790553989228223793461834703506872747071705167995972707253940099469869516422893633357693");
     cpp_int q("204616454475328391399619135615615385636808455963116802820729927402260635621645177248364272093977747839601125961863785073671961509749189348777945177811");
 
+    cpp_int e,d,phi,m,n;
 
     n = p*q;
     phi = (p-1)*(q-1);
 
     e = getRandomR(phi);
-    while(Egcd(phi,e)!=1){
+    while(egcd(e,phi)!=1){
         e = getRandomR(phi);
     }
 
     d = modInv(e,phi);
 
-    cpp_int m = 10;
+    //messages m1,m2
+    cpp_int m1=10,m2=12;
 
-    cpp_int sign,verify;
+    //encryption
+    cpp_int enc1,enc2,enc;
 
-    sign = power(m,d,n);
+    enc1 = power(m1,e,n);
+    enc2 = power(m2,e,n);
 
-    verify = power(sign,e,n);
+    // product
+    enc = enc1*enc2;    // (m1*m2) mod n
 
-    cout<<sign<<endl<<verify<<endl;
+    //decryption
+    cpp_int dec1,dec2,dec;
+    dec1 = power(enc1,d,n); // decrypted message m1
+    dec2 = power(enc2,d,n); // decrypted message m2
 
+    dec = power(enc,d,n); //decrypted enc^d % n = m1*m2
 
+    cout<<dec<<endl;
+
+    return 0;
 }
